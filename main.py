afad065d7f66f4b19c64cdd071d0954bd1522195
Venia04 <37250188+Venia04@users.noreply.github.com>
2022-03-30 14:48:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import telebot
 import config
-
-
+from telebot import types
 bot = telebot.TeleBot(config.TOKEN)
-bot.remove_webhook()
+
+button1 = 'Button 1'
+button2 = 'Button 2'
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id,"Привет, я бот для записи в NeoCyber!")
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+	markup=types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	item1=types.KeyboardButton(button1)
+	markup.add(item1)
+	markup.add(item1)
+	bot.send_message(message.chat.id,'Выберите вариант', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
-def firstFunc(message):
-	bot.send_message(message.chat.id, message.text)
-	print(str(message.from_user.username) + " Message ID:" + str(message.id))
+# def firstFunc(message):
+# 	bot.send_message(message.chat.id, message.text)
+# 	print(str(message.from_user.username) + " Message ID:" + str(message.id))
+def message_reply(message):
+	if message.text == button1:
+		bot.send_message(message.chat.id,"Ти лох)")
+
 
 bot.polling(none_stop=True)
