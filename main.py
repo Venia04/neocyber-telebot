c0c6a04d00185f748189973e004bc76ebfc9287c
Venia04 <kalmansteve15@gmail.com>
2022-05-06 22:44:05 +0300

    some changes and gitignore

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ import config
 from telebot import types
 bot = telebot.TeleBot(config.TOKEN)
 
-button1 = 'Button 1'
+make_appointment = 'Записаться'
 button2 = 'Button 2'
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id,"Привет, я бот для записи в NeoCyber!")
+	bot.send_message(message.chat.id,f"*Привет, {message.from_user.first_name}!*\
+	\nЭто бот для записи в компьютерном клубе NeoCyber!\nДля записи просто\
+	нажми кнопку *Записаться* внизу⬇\nА для отмены уже существующей записи позвони\
+	админу клуба по номеру +375-29-123-45-67", parse_mode="Markdown")
+	markup=types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+	item1=types.KeyboardButton(make_appointment)
+	markup.add(item1)
+	bot.send_message(message.chat.id, 'Выберите вариант', reply_markup=markup)
+
 
 @bot.message_handler(commands=['button'])
 def button_message(message):
-	markup=types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-	item1=types.KeyboardButton(button1)
-	markup.add(item1)
-	markup.add(item1)
-	bot.send_message(message.chat.id,'Выберите вариант', reply_markup=markup)
+	pass
+
 
 @bot.message_handler(content_types=['text'])
-# def firstFunc(message):
-# 	bot.send_message(message.chat.id, message.text)
-# 	print(str(message.from_user.username) + " Message ID:" + str(message.id))
 def message_reply(message):
-	if message.text == button1:
-		bot.send_message(message.chat.id,"Ти лох)")
+	if message.text == make_appointment:
+		bot.send_message(message.chat.id, "")
 
 
 bot.polling(none_stop=True)
