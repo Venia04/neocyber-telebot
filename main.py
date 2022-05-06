671a2a78f1f8949d886485a06c6de1c35b73b869
Venia04 <kalmansteve15@gmail.com>
2022-05-07 00:58:09 +0300

    Menu changes

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
+from markups import Keyboard
 import telebot
 import config
 from telebot import types
-bot = telebot.TeleBot(config.TOKEN)
 
-make_appointment = 'Записаться'
-button2 = 'Button 2'
+bot = telebot.TeleBot(config.TOKEN)
+keyboard = Keyboard(bot)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id, f'*Привет, {message.from_user.first_name}!*\
+	user_markup = types.ReplyKeyboardMarkup(True, False)
+
+	user_markup.row('Записаться')
+
+	hello_message = f'*Привет, {message.from_user.first_name}!*\
 	\nЭто бот для записи в компьютерном клубе NeoCyber!\nДля записи просто\
 	нажми кнопку *Записаться* внизу⬇\nА для отмены уже существующей записи позвони\
-	админу клуба по номеру +375-29-123-45-67', parse_mode="Markdown")
-	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-	item1 = types.KeyboardButton(make_appointment)
-	markup.add(item1)
-	bot.send_message(message.chat.id, 'Выберите вариант', reply_markup=markup)
+	админу клуба по номеру +375-29-123-45-67'
+
+	bot.send_message(message.chat.id, hello_message, reply_markup=user_markup, parse_mode='Markdown')
 
 
-@bot.message_handler(commands=['button'])
-def button_message(message):
-	pass
+@bot.message_handler(func=lambda mess: "Главное меню" == mess.text, content_types=['text'])
+def handle_text(message):
+	keyboard.main_menu(message)
 
 
-@bot.message_handler(content_types=['text'])
-def message_reply(message):
-	if message.text == make_appointment:
-		bot.send_message(message.chat.id, "")
+@bot.message_handler(func=lambda mess: "Записаться" == mess.text, content_types=['text'])
+def handle_text(message):
+	keyboard.make_appointment(message)
 
 
-bot.polling(none_stop=True)
+bot.polling(non_stop=True)
