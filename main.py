bef0ed98bf9c6a349b89dd32d552730a13a52e78
Venia04 <kalmansteve15@gmail.com>
2022-09-16 14:56:33 +0300

    reformat

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ keyboard = Keyboard(bot)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
 
-	user_markup.row('Записаться')
+    user_markup.row('Записаться')
 
-	hello_message = f'*Привет, {message.from_user.first_name}!*\
+    hello_message = f'*Привет, {message.from_user.first_name}!*\
 	\nЭто бот для записи в компьютерном клубе NeoCyber!\nДля записи просто\
 	нажми кнопку *Записаться* внизу⬇\nА для отмены уже существующей записи позвони\
 	админу клуба по номеру +375-29-123-45-67'
 
-	bot.reply_to(message, hello_message, reply_markup=user_markup, parse_mode='Markdown')
+    bot.reply_to(message, hello_message, reply_markup=user_markup, parse_mode='Markdown')
 
 
 @bot.message_handler(func=lambda mess: "Главное меню" == mess.text, content_types=['text'])
 def handle_text(message):
-	keyboard.main_menu(message)
+    keyboard.main_menu(message)
 
 
 @bot.message_handler(func=lambda mess: "Записаться" == mess.text, content_types=['text'])
 def handle_text(message):
-	keyboard.make_appointment(message)
+    keyboard.make_appointment(message)
 
 
 bot.polling(non_stop=True)
