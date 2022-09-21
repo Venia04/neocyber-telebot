3b8b21d2a4f4ddb64a7b137f53ed671f8497bcc5
Venia04 <kalmansteve15@gmail.com>
2022-09-21 21:38:13 +0300

    Добавлены функции обработки базы данных, начало админ-панели

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-from uuid import uuid4
-from markups import Keyboard
-import telebot
-
+# import logging
+import sqlite3
 import config
+import telebot
+from markups import Keyboard
 from telebot import types
 
+#
+# from uuid import uuid4
+# logger = telebot.logger
+# telebot.logger.setLevel(logging.DEBUG)
+ADMIN = config.ADMIN
+# путь к БД: рядом с папкой проекта папка neoDB
+DB_PATH = '../neoDB/appoint.db'
+conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+cursor = conn.cursor()
 bot = telebot.TeleBot(config.TOKEN)
 keyboard = Keyboard(bot)
 
 
+# функция добавления информации в базу данных
+def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
+    cursor.execute('INSERT OR IGNORE INTO users (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)',
+                   (user_id, user_name, user_surname, username))
+    conn.commit()
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    user_markup = types.ReplyKeyboardMarkup(True, False)
-
-    user_markup.row('Записаться')
-
-    hello_message = f'*Привет, {message.from_user.first_name}!*\
-	\nЭто бот для записи в компьютерном клубе NeoCyber!\nДля записи просто\
-	нажми кнопку *Записаться* внизу⬇\nА для отмены уже существующей записи позвони\
-	админу клуба по номеру +375-29-123-45-67'
-
-    bot.reply_to(message, hello_message, reply_markup=user_markup, parse_mode='Markdown')
+    uid = message.from_user.id
+    # if uid == ADMIN:
+    #     keyboard.admin_func(message)
+    # else:
+    #     keyboard.main_menu(message)
+    keyboard.main_menu(message)
+    uname = message.from_user.first_name
+    usname = message.from_user.last_name
+    username = message.from_user.username
+    db_table_val(user_id=uid, user_name=uname, user_surname=usname, username=username)
+    print(f"{uname}: {uid}")
+    print('SUCCESS start command')
 
 
 @bot.message_handler(func=lambda mess: "Главное меню" == mess.text, content_types=['text'])
@@ -33,4 +51,8 @@ def handle_text(message):
     keyboard.make_appointment(message)
 
 
-bot.polling(non_stop=True)
+def run():
+    bot.polling(non_stop=True)
+
+
+run()
--- markups.py
+++ markups.py
@@ -2,21 +2,34 @@ import telebot
 
 
 class Keyboard:
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	def main_menu(self, message):
-		user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-		user_markup.row('Записаться')
-		hello_message = f'*Привет, {message.from_user.first_name}!*\
-			\nЭто бот для записи в компьютерном клубе NeoCyber!\nДля записи просто\
-			нажми кнопку *Записаться* внизу⬇\nА для отмены уже существующей записи позвони\
-			админу клуба по номеру +375-29-123-45-67'
-		self.bot.send_message(message.from_user.id, hello_message, reply_markup=user_markup, parse_mode='Markdown')
+    def main_menu(self, message):
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+        user_markup.row('Записаться')
+        hello_message = f'*Привет, {message.from_user.first_name}!*\
+\nЭто бот для записи в компьютерном клубе NeoCyber!\nДля записи просто \
+нажми кнопку *Записаться* внизу⬇\nА для отмены уже существующей записи позвони \
+админу клуба по номеру +375-29-123-45-67'
+        self.bot.send_message(message.from_user.id, hello_message,
+                              reply_markup=user_markup, parse_mode='Markdown')
 
-	def make_appointment(self, message):
-		user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
-		user_markup.row('Компьютер')
-		user_markup.row('Playstation')
-		user_markup.row('VIP Компьютер')
-		self.bot.send_message(message.from_user.id, 'Выберите Пакет:', reply_markup=user_markup)
+    def make_appointment(self, message):
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+        user_markup.row('Компьютер')
+        user_markup.row('Playstation')
+        user_markup.row('VIP Компьютер')
+        self.bot.send_message(message.from_user.id, 'Выберите Пакет:',
+                              reply_markup=user_markup)
+
+    def admin_func(self, message):
+        kb = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        types = telebot.types
+        kb.add(types.InlineKeyboardButton(text="Это"))
+        kb.add(types.InlineKeyboardButton(text="тестовая"))
+        kb.add(types.InlineKeyboardButton(text="функция"))
+        kb.add(types.InlineKeyboardButton(text="для проверки."))
+        self.bot.send_message(message.from_user.id, 'Добро пожаловать в Админ-панель!',
+                              reply_markup=kb)
+
